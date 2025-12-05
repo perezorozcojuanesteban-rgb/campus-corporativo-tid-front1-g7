47cdb77b5dffb86c4c0317da937f967c13c9aeb3
Juan <perezorozcojuanesteban@gmail.com>
2025-12-05 15:46:16 -0500

    Correccion error en detalle curso por categorias

--- src/controllers/busqueda.js
+++ src/controllers/busqueda.js
@@ -14,6 +14,7 @@ export function activarBusqueda() {
             curso.titulo.toLowerCase().includes(texto)
         );
 
+
         contenedor.innerHTML = "";
 
         filtrados.forEach(curso => {
--- src/models/modelo-cursos.js
+++ src/models/modelo-cursos.js
@@ -1,11 +1,11 @@
+// src/models/modelo-cursos.js
 export const cursos = [{
         id: 1,
         titulo: "Introducción a HTML",
         categoria: "Tecnología",
         descripcion: "Aprende las bases del desarrollo web con HTML y crea tus primeras páginas.",
         video: "https://www.youtube.com/embed/lZeH53-EdjA?si=YFTTH6Y_WvwRsG36",
-        pagina: "./src/pages/cursos/introduccion-html.html"
-
+        pagina: "/src/pages/cursos/introduccion-html.html"
     },
     {
         id: 2,
@@ -13,7 +13,7 @@ export const cursos = [{
         categoria: "Tecnología",
         descripcion: "Aprende a darle estilo a sitios web y mejorar su presentación visual.",
         video: "https://www.youtube.com/embed/3yM5uXp-T_0?si=GUX_tXO8ubmfXZpm",
-        pagina: "./src/pages/cursos/diseno-css.html"
+        pagina: "/src/pages/cursos/diseno-css.html"
     },
     {
         id: 3,
@@ -21,7 +21,7 @@ export const cursos = [{
         categoria: "Gestión",
         descripcion: "Desarrolla habilidades de liderazgo y gestión de equipos de trabajo.",
         video: "https://www.youtube.com/embed/xtc6cqg49EA?si=iZWi8_qL4jUFNwrL",
-        pagina: "./src/pages/cursos/gestion-empresarial.html"
+        pagina: "/src/pages/cursos/gestion-empresarial.html"
     },
     {
         id: 4,
@@ -29,7 +29,7 @@ export const cursos = [{
         categoria: "Tecnología",
         descripcion: "Desarrolla habilidades para la creación y manejo de bases de datos.",
         video: "https://www.youtube.com/embed/N7qupl0X4Jk?si=zM-sSqErVpRccOpx",
-        pagina: "./src/pages/cursos/gestion-basedatos.html"
+        pagina: "/src/pages/cursos/gestion-basedatos.html"
     },
     {
         id: 5,
@@ -37,7 +37,7 @@ export const cursos = [{
         categoria: "Gestión",
         descripcion: "Aprende a coordinar equipos de alto rendimiento y mejorar dinámicas laborales.",
         video: "https://www.youtube.com/embed/2MocfNK-Fg0?si=RCElZRnn6UoCyWGf",
-        pagina: "./src/pages/cursos/liderazgo.html"
+        pagina: "/src/pages/cursos/liderazgo.html"
     },
     {
         id: 6,
@@ -45,7 +45,7 @@ export const cursos = [{
         categoria: "Gestión",
         descripcion: "Domina conceptos clave de contabilidad, presupuestos y decisiones estratégicas.",
         video: "https://www.youtube.com/embed/fUYDODrjj28?si=g38Fek1FiQLMk-GC",
-        pagina: "./src/pages/cursos/gestion-financiera.html"
+        pagina: "/src/pages/cursos/gestion-financiera.html"
     },
     {
         id: 7,
@@ -53,7 +53,7 @@ export const cursos = [{
         categoria: "Innovación",
         descripcion: "Aprende a proteger la información y prevenir ataques digitales.",
         video: "https://www.youtube.com/embed/3G8yJaudGC0?si=lI0eC134OPx_iRFj",
-        pagina: "./src/pages/cursos/ciberseguridad.html"
+        pagina: "/src/pages/cursos/ciberseguridad.html"
     },
     {
         id: 8,
@@ -61,14 +61,14 @@ export const cursos = [{
         categoria: "Innovación",
         descripcion: "Explora el impacto del blockchain en la trazabilidad y gestión de datos.",
         video: "https://www.youtube.com/embed/H2vOgrH7mgw?si=4HGgGCK0H6yXz4Fd",
-        pagina: "./src/pages/cursos/blockchain.html"
+        pagina: "/src/pages/cursos/blockchain.html"
     },
     {
         id: 9,
         titulo: "Inteligencia Artificial",
         categoria: "Innovación",
-        descripcion: "Conoce las bases de la IA y su impacto en la automatización empresarial.",
+        descripcion: "Conoce las bases del la IA y su impacto en la automatización empresarial.",
         video: "https://www.youtube.com/embed/8lMIdrlIWOQ?si=HymJ5ilTj85OnVfF",
-        pagina: "./src/pages/cursos/ia.html"
+        pagina: "/src/pages/cursos/ia.html"
     }
 ];
